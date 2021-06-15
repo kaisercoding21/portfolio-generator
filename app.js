29f3e836a1783f47bc84f3540cd3de6eee1d5eb6
Kaisercoding <kaisercoding@outlook.com>
2021-06-15 18:59:50 -0400

    added inquirer package

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+  ])
+  .then((answers) => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+// fs.writeFile('index.html', generatePage(name, github), (err) => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
+// ================================================== original old code
 // const printProfileData = profileDataArr => {
 //   // This...
 //   for (let i = 0; i < profileDataArr.length; i += 1) {
