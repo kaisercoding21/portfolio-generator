03f33d166e8afd7ef4285bc94a119f6808e72925
Kaisercoding <kaisercoding@outlook.com>
2021-06-17 16:17:54 -0400

    added ability to handle more data from user

--- app.js
+++ app.js
@@ -144,27 +144,16 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(projectAnswers);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        'Portfolio complete! Check out index.html to see the output!'
+      );
+    });
   });
-// .then((projectData) => {
-//   portfolioData.projects.push(projectData);
-//   if (projectData.confirmAddProject) {
-//     return promptProject(portfolioData);
-//   } else {
-//     return portfolioData;
-//   }
-// });
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', generatePage(name, github), (err) => {
-//   if (err) throw new Error(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 // ================================================== original old code
 // const printProfileData = profileDataArr => {
